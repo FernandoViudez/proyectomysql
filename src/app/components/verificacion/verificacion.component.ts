766d56a1c7d2f933928bfe6aa7c55b8cd0415d30
marcos <marcosfgk@gmai.com>
2020-09-09 18:59:57 -0300

    no termine aun

--- src/app/components/verificacion/verificacion.component.ts
+++ src/app/components/verificacion/verificacion.component.ts
@@ -68,8 +68,25 @@ export class VerificacionComponent implements OnInit {
             icon: "error"
           })
         } else {
-          return Swal.fire({
-            title: "CORRECTO"
+          let timerInterval
+          Swal.fire({
+            title: 'Auto close alert!',
+            html: 'I will close in <b></b> milliseconds.',
+            timer: 2000,
+            timerProgressBar: true,
+            onBeforeOpen: () => {
+              Swal.showLoading()
+              timerInterval = setInterval(() => {
+              }, 100)
+            },
+            onClose: () => {
+              clearInterval(timerInterval)
+            }
+          }).then((result) => {
+            /* Read more about handling dismissals below */
+            if (result.dismiss === Swal.DismissReason.timer) {
+              console.log('I was closed by the timer')
+            }
           })
         }
 
