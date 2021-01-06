79876c044dd672f5138a3a9c93272552938f02cd
Fernando <viudezfernando@gmail.com>
2021-01-06 16:04:14 -0300

    Add serverdt to list of hostnames

--- src/app/_utils/generarBackPath.ts
+++ src/app/_utils/generarBackPath.ts
@@ -3,7 +3,9 @@ export let obtenerPath = () => {
     
     let url: string;
  
-    if (window.location.hostname == 'revestadt.sytes.net') {
+    if ( window.location.hostname == 'revestadt.sytes.net' 
+    || window.location.hostname == 'serverdt' 
+    ) {
         url = "http://pcweb:8080/api/";
     } else {
         url = "http://localhost:8080/api/";
