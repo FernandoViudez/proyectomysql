c90aae4c512170e34cb24938e5c41f4c9c97e12e
Fernando <viudezfernando@gmail.com>
2021-01-06 10:27:07 -0300

    8080

--- src/app/_utils/generarBackPath.ts
+++ src/app/_utils/generarBackPath.ts
@@ -4,7 +4,7 @@ export let obtenerPath = () => {
     let url: string;
  
     if (window.location.hostname == 'revestadt.sytes.net') {
-        url = "http://pcweb:8083/api/";
+        url = "http://pcweb:8080/api/";
     } else {
         url = "http://localhost:8080/api/";
     }
